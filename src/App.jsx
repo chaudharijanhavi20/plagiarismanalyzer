2537a1fa7d7ad61e725889a3cdb3d0615fd118e0
chaudharijanhavi20 <cm.a.20.janhavi.chaudhari@gmail.com>
2023-03-19 12:44:45 +0530

    cards

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import Crd from './component/Crd';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <Appcontainer>
+    <Crd/>
+    <Crd/>
+    <Crd/>
+    <Crd/>
+    <Crd/>
+    <Crd/>
+    </Appcontainer>
+  );
+}
+const Appcontainer=styled.div`
+display:flex;
+width:100%;
+flex-wrap:wrap;
+align-items:center;
+justify-content:space-evenly;
+gap:1rem;`
+
+
+export default App;
---
+++ src/component/Crd.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './crd.css';
+
+function Crd() {
+  return (
+    <div className='crd'>
+   <div className='card'>
+    <div className='upper-container'>
+        <div className='image'>
+        <img src='https://cdn-icons-png.flaticon.com/512/5100/5100994.png' alt='' height='100px' width='100px'/>
+        </div> 
+    </div>
+    <div className='lower-container'>
+    <p>Plagiarism Checker X is one of the most reliable and easy-to-use plagiarism detectors on the market. </p>
+        <button>Upload file</button>
+    </div>
+   </div>
+   </div>
+  )
+}
+
+export default Crd
