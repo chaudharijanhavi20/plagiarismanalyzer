e0d7ea190a3d0ed22156a1b2927011c5e137d614
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2023-03-23 01:49:58 +0530

    Fixed issue

--- src/component/File.jsx
+++ src/component/File.jsx
@@ -1,25 +1,27 @@
-import { createStyles, SegmentedControl, rem } from '@mantine/core';
-
+import { createStyles, SegmentedControl, rem } from "@mantine/core";
+import * as React from "react";
+import { MyContext } from "../App";
 const useStyles = createStyles((theme) => ({
   root: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.white,
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
     boxShadow: theme.shadows.md,
     border: `${rem(1)} solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[1]
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[1]
     }`,
   },
 
   indicator: {
-    backgroundImage: theme.fn.gradient({ from: '#865DFF', to: 'black' }),
+    backgroundImage: theme.fn.gradient({ from: "#865DFF", to: "black" }),
   },
 
   control: {
-    border: '0 !important',
+    border: "0 !important",
   },
 
   label: {
-    '&, &:hover': {
-      '&[data-active]': {
+    "&, &:hover": {
+      "&[data-active]": {
         color: theme.white,
       },
     },
@@ -27,12 +29,55 @@ const useStyles = createStyles((theme) => ({
 }));
 
 export default function File() {
+  const {
+    urlobj,
+    seturlobj,
+    open,
+    setOpen,
+    setscore,
+    setshowscore,
+    showreference,
+    setshowreference,
+    referencedata,
+    setreferencedata,
+    scannedtext,
+    setscannedtext,
+    showscannedtext,
+    showsetscannedtext,
+  } = React.useContext(MyContext);
+  const setdocs = () => {
+    const currentset = document.querySelector(
+      ".mantine-SegmentedControl-controlActive .mantine-SegmentedControl-input"
+    )?.value;
+    const doc = document.querySelector(
+      ".mantine-SegmentedControl-controlActive"
+    );
+    console.log(doc);
+    doc?.addEventListener("click", () => {
+      setOpen(!open);
+      const updatedObject = Object.keys(urlobj).reduce((result, key) => {
+        if (key === currentset) {
+          result[key] = true;
+        } else {
+          result[key] = false;
+        }
+        return result;
+      }, {});
+      seturlobj(updatedObject);
+      console.log(urlobj);
+    });
+  };
+  // doc.addEventListener('click',()=>{
+  //   console.log(doc)
+  // })
   const { classes } = useStyles();
   return (
     <SegmentedControl
+      onClick={setdocs}
       radius="xl"
       size="xl"
-      data={[ 'Get Reference', 'PDF Merge', 'Scanned pdf', 'Extrac timage']}
+      style={{ display: "flex", flexWrap: "wrap" }}
+      data={["getreference", "pdfmerge", "scannedpdf", "extractimage"]}
       classNames={classes}
     />
   );
